9a3a53a9a9cc6273b45cb002370de9d194fe339b
Ruth <ruthvazquezibanez@gmail.com>
2026-09-09 18:02:32 -0500

    feat: Creación de backend usando Prisma ORM y PostgreSQL

---
+++ server/src/routes/product.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import * as productController from '../controllers/product.controller.js'
+
+const router = Router();
+
+router.get('/', productController.getProducts);
+router.post('/', productController.createProduct);
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
+
+export default router;
---
+++ src/services/product.service.ts
@@ -0,0 +1,32 @@
+const API_URL = '/api/products';
+
+export async function getProducts() {
+    const res = await fetch(API_URL);
+    if (!res.ok) throw new Error('Failed to fetch products');
+    return res.json();
+}
+
+export async function createProduct(data: {name:string; description:string; image_url:string; price:number; }) {
+    const res = await fetch(API_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    });
+    if (!res.ok) throw new Error('Failed to create product');
+    return res.json();
+}
+
+export async function updateProduct(id: number, data: Partial<{name:string; description:string; image_url:string; price:number; }>) {
+    const res = await fetch(`${API_URL}/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    });
+}
+
+export async function deleteProduct(id: number) {
+    const res = await fetch(`${API_URL}/${id}`, {
+        method: 'DELETE'
+    });
+    if (!res.ok) throw new Error('Failed to delete product');
+}
